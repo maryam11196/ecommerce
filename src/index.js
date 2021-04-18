a5d8b459fea28844db2e2c4b039ce64dcef9fc79
maryam <maryam.mouhajer.96@gmail.com>
2021-04-18 17:35:35 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '/css/style.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+console.log("مرحبا ");
